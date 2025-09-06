0de20abead2998b4568fe864327a2ea823f1e64e
Balázs Imre Tamás <tmsblzs@gmail.com>
2025-09-06 10:08:48 +0200

    Add PaymentController and unit tests.

---
+++ src/Nop.Plugin.Payments.SimplePay/ViewModels/PaymentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Nop.Core.Domain.Orders;
+using Nop.Core.Domain.Stores;
+
+namespace Nop.Plugin.Payments.SimplePay.ViewModels;
+public class PaymentViewModel
+{
+    public Store CurrentStore { get;set; }
+    public Order Order { get; set; }
+    public string PaymentUrl { get; set; }
+}
